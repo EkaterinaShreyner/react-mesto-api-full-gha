aca594b55f91468fd0bb75bbb8ba34e2a8949cac
Ekaterina Shreyner <kkkatya88@yandex.ru>
2023-08-06 16:18:16 +0400

    fix port

--- backend/app.js
+++ backend/app.js
@@ -12,11 +12,13 @@ const errorsHandler = require('./middlewares/errorsHandler');
 
 const NotFoundError = require('./errors/NotFoundError');
 
-const { PORT = 4000 } = process.env;
+// const { PORT = 4000 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 // поддержка cors, разрешенные источники
-app.use(cors({ origin: 'http://localhost:3000' }));
+// app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'http://localhost:3001' }));
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
