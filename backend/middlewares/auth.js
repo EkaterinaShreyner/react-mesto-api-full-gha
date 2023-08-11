e4b35db1119ec2ac0926f43d15b5404e07be9000
Ekaterina Shreyner <kkkatya88@yandex.ru>
2023-08-11 17:34:22 +0400

    fix .env

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,6 +2,8 @@
 const jwt = require('jsonwebtoken'); // для создания токена
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 // eslint-disable-next-line consistent-return
 function auth(req, _res, next) {
   // достаём авторизационный заголовок
@@ -19,7 +21,7 @@ function auth(req, _res, next) {
 
   try {
     // попытаемся верифицировать токен
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     // отправим ошибку, если не получилось
     console.log(err);
